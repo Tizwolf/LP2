d2210de6505d38e708cee31bb3dfdd92e8ad1f5c
Tizwolf <37790640+Tizwolf@users.noreply.github.com>
2019-03-29 23:13:24 -0500

    Update Selection_sort(puntero).cpp

--- Métodos de ordenamiento/Selection_sort(puntero).cpp
+++ Métodos de ordenamiento/Selection_sort(puntero).cpp
@@ -1,48 +1,67 @@
 
-//SELECTION SORT
+//SELECTION
+//void <fun>_sort (int *A, int n)
 
-#include<iostream>
+
+#include <iostream>
+
+
 using namespace std;
 
-void selection_sort (int  A[], int n) 
+void selection_sort(int *A, int n)
 {
-        int min,i,j,aux;
-        for (i=0; i<n-1; i++) 
-        {
-              min=i;
-              for(j=i+1; j<n; j++)
+	int aux;	
+	for (int i = 0; i<n-1 ; i++){
+		
+		int min = i;
+		
+        for(int j =i+1; j<n; j++)
                     if(A[min] > A[j])
                        min=j;
               aux=A[min];
               A[min]=A[i];
               A[i]=aux ;
-        }
- 
-} 
-
-
-int main () 
-{
-	int n;
-    int A[n];
+		
+		/*
+		
+		for(int j=i+1; j<n; j++){
+			
+			if(*(A+min) > *(A+j)){
+				min = *(A+j);			
+				
+			}		
+		}
+		aux = *(A+min);
+		*(A+min) = *(A+i);
+		*(A+i) = aux;
+		*/
+	}
 	
-     
-    cout<<"Numero de elementos: ";
-	cin>>n;
-      
-    for(int i=0;i<n;i++)
-    {
-        cout<<"Ingresa numero "<<i+1<<": ";
-        cin>>A[i];
-    }
-         
-    
-    selection_sort(A,n);
+	 
+	
+}
 
+int main (){
+	int n; //numero de elementos del array
+	int *A;
+	cout<<"Digite el numero de elementos: ";
+	cin>>n;
+	
+	A = new int[n];
+	
+	//entra elementos del array
+	for(int i=0;i<n;i++){
+		cout<<"Digite un numero ["<<i<<"]: ";
+		cin>>*(A+i);
+	}
+				
+	selection_sort(A,n);
+				
 	//Imprime los elementos ordenados del array e.e				
 	for(int i=0;i<n;i++){
-		cout<<A[i]<<" ";
+		cout<<*(A+i)<<" ";
 	}			
-	
+	delete[] A;
 	return 0;
+
 }
