9a897bfb1626fee89c75317f1c2a7e6bf8a792c2
Tizwolf <37790640+Tizwolf@users.noreply.github.com>
2019-04-06 23:26:03 -0500

    Create Prices of books.cpp

---
+++ UVA/Prices of books.cpp
@@ -0,0 +1,52 @@
+
+
+///UVA 2          
+//Books and prices
+
+#include <iostream>
+#include <cmath> 
+#include <stdio.h>
+
+
+using namespace std;
+
+ int main(){
+ 	
+ 	int nbooks; 
+  	
+	int money; //dinero cada 2 semanas
+ 	
+ //	while ( nbooks>= 2 && nbooks<= 10000){
+	 
+	 	while (cin>>nbooks){
+	 		
+	 		int prices[nbooks]={};
+	 		
+	 		for (int k = 0; k<nbooks; k++){
+	 			cin>>prices[k];
+			 	
+			 }
+	 		 cin>>money;
+	 			 		
+	 		int i, j; //precios de 2 libros
+	 			
+	 		for ( i=0; i<nbooks; i++)
+	 		{
+	 			for (j=1; j<nbooks ; j++){
+				 	
+	 				if (prices[i] + prices [j] == money){
+	 					if (prices[i] <= prices[j]){
+	 						
+		 					cout<<"Peter should buy books whose prices are "<<prices[i]<<" and "<<prices[j]<<endl;
+						
+						}
+					}
+					
+				}
+			}
+		 	
+		}
+ //	}
+ 	return 0; 
+ 	
+ }
